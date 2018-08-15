f3e3f5797344679de0e37a76a886a2fd09ef5905
Deven Desai <deven.desai.amd@gmail.com>
2018-08-15 10:49:35 -0700

    update 180815 - rack-10

--- tensorflow/run_unit_test_using_bazel.sh
+++ tensorflow/run_unit_test_using_bazel.sh
@@ -1,18 +1,22 @@
-# cat $1 | while read testname
-# do
-#     bazel test --test_env=HIP_VISIBLE_DEVICES=0 --test_sharding_strategy=disabled --cache_test_results=no --config=opt --config=rocm $testname
-# done
 
+# cat $1 | while read testname
+# for i in {1..10}
+
+testname=$1
+
+# do
 bazel test  \
       --test_env=HIP_VISIBLE_DEVICES=0 \
       --test_sharding_strategy=disabled \
       --cache_test_results=no \
       --config=opt \
       --config=rocm \
-      $1
+      $testname
+# done
 
-#      --test_env=HIP_TRACE_API=2 \
-#      --test_env=MIOPEN_ENABLE_LOGGIN=1 \
-#      --test_env=HCC_DB=0x48a \
-#      --test_env=TF_CPP_MIN_VLOG_LEVEL=1 \
-#      --test_env=TF_CPP_MIN_LOG_LEVEL=1 \
+      # --test_env=TF_CPP_MIN_VLOG_LEVEL=1 \
+      # --test_env=TF_CPP_MIN_LOG_LEVEL=1 \
+      # --test_env=HCC_DB=0x48a \
+      # --test_env=MIOPEN_ENABLE_LOGGING=1 \
+      # --test_env=MIOPEN_ENABLE_LOGGING_CMD=1 \
+      # --test_env=HIP_TRACE_API=2 \
