5e820e18f8b736531fd4b200ffecc8e63453b0a2
Deven <deven.desai@gmail.com>
2019-07-25 20:22:34 -0400

    6 evelyn update

--- docker/get_docker_image.sh
+++ docker/get_docker_image.sh
@@ -1,28 +1,30 @@
 # docker_repo=rocm/eigen-test
-docker_repo=rocm/tensorflow
+# docker_repo=rocm/tensorflow
 # docker_repo=rocm/tensorflow-autobuilds
 # docker_repo=rocm/tensorflow-private
 #
 # docker_repo=devenamd/tensorflow
+docker_repo=devenamd/ubuntu
 #
 # docker_repo=sunway513/hiptensorflow
 
 
-tag=latest
+# tag=latest
+tag=develop.16.04
 
 docker_image=$docker_repo:$tag
 
-container_name=deven_rocm26_release_baseline
+container_name=deven_MLIR
 
-docker run \
-       -it \
-       --network=host \
-       --device=/dev/kfd \
-       --device=/dev/dri \
-       --group-add video \
-       --cap-add=SYS_PTRACE \
-       --security-opt seccomp=unconfined \
-       -v $HOME/deven/common:/common \
-       -v $HOME/deven/dockerx:/dockerx \
-       --name $container_name \
-       $docker_image
+options=""
+options="$options -it"
+options="$options --network=host"
+# options="$options --device=/dev/kfd"
+# options="$options --device=/dev/dri"
+# options="$options --group-add video"
+options="$options --cap-add=SYS_PTRACE"
+options="$options --security-opt seccomp=unconfined"
+options="$options -v $HOME/deven/common:/common"
+
+docker run $options --name $container_name $docker_image
+
