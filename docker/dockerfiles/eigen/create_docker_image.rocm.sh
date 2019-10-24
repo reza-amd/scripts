cf72dc91b730bff4deba0b62a70768f8d93fc565
Deven Desai <deven.desai.amd@gmail.com>
2019-10-24 19:57:05 +0000

    ixt-rack-04 update

--- docker/dockerfiles/eigen/create_docker_image.rocm.sh
+++ docker/dockerfiles/eigen/create_docker_image.rocm.sh
@@ -1,2 +1,2 @@
-image_name=rocm/eigen-test:rocm-2.6
+image_name=rocm/eigen-test:rocm-2.8
 docker build -t $image_name -f Dockerfile.rocm .
--- docker/get_docker_image.sh
+++ docker/get_docker_image.sh
@@ -1,34 +1,41 @@
 # docker_repo=rocm/eigen-test
-# docker_repo=rocm/tensorflow
-# docker_repo=rocm/tensorflow-autobuilds
-# docker_repo=rocm/tensorflow-private
-#
-# docker_repo=devenamd/tensorflow
-docker_repo=devenamd/mlir
-#
-# docker_repo=sunway513/hiptensorflow
+# tag=rocm-2.8
+# tag=rocm-2.6
 
-tag=rocm-2.8-latest
+# docker_repo=rocm/tensorflow
+#
+
+# docker_repo=rocm/tensorflow-autobuilds
 # tag=rocm2.8-c750a36
 # tag=rocm2.6-2704edd
-# tag=latest
-# tag=develop.16.04
-# tag=fused_batchnorm_bug
-# tag=rocm2.6-tf1.13-miopen1.8.0-alexnet-triage
 
+# docker_repo=rocm/tensorflow-private
+#
+
+# docker_repo=devenamd/tensorflow
+# tag=fused_batchnorm_bug
+
+# docker_repo=devenamd/mlir
+# tag=rocm-2.6-latest
+
+# docker_repo=sunway513/hiptensorflow
+#
 
 docker_image=$docker_repo:$tag
 
-container_name=deven_rocm28_MLIR
+container_name=deven_rocm28_tf_misc_01
 
 options=""
 options="$options -it"
 options="$options --network=host"
+options="$options --ipc=host"
+options="$options --shm-size 16G"
 options="$options --device=/dev/kfd"
 options="$options --device=/dev/dri"
 options="$options --group-add video"
 options="$options --cap-add=SYS_PTRACE"
 options="$options --security-opt seccomp=unconfined"
 options="$options -v $HOME/deven/common:/common"
+# options="$options -v /data/imagenet-inception:/imagenet"
 
 docker run $options --name $container_name $docker_image
