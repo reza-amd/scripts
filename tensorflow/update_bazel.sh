2958c0e645b1c18bafd07281e808e4710d00dd7d
Deven Desai <deven.desai.amd@gmail.com>
2019-12-05 08:41:51 -0800

    update bazel version to 1.1.0

--- tensorflow/update_bazel.sh
+++ tensorflow/update_bazel.sh
@@ -3,9 +3,10 @@
 #version=0.20.0
 #version=0.23.2
 #version=0.24.1
-version=0.26.1
+#version=0.26.1
 #version=0.27.1
 #version=0.29.1
+version=1.1.0
 cd /root/
 wget https://github.com/bazelbuild/bazel/releases/download/$version/bazel-$version-installer-linux-x86_64.sh
 chmod a+x bazel-$version-installer-linux-x86_64.sh
