9c78f66912c2e6116b849b79d5fa61a7792cb679
Deven Desai <deven.desai.amd@gmail.com>
2020-06-13 21:52:35 +0000

    ixt-hq-99 update

--- tensorflow/run_and_collect_benchmark_data.py
+++ tensorflow/run_and_collect_benchmark_data.py
@@ -14,17 +14,21 @@ from openpyxl.styles import Font
 
 os.environ["HIP_VISIBLE_DEVICES"]="0";
 
-def run_shell_command(cmd):
-    print (" ".join(cmd))
-    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # if result.returncode != 0:
-    #     print("FAILED - {}".format(" ".join(cmd)))
-    #     print("         {}".format(result.stdout.decode()))
-    #     print("         {}".format(result.stderr.decode()))
+def run_shell_command(cmd, env_vars):
+
+    all_env_vars = " ".join(["{}={}".format(key, value) for key, value in env_vars.items()])
+    shell_cmd = " ".join(cmd)
+    full_cmd = " ".join([all_env_vars, shell_cmd])
+    print(full_cmd)
+    result = subprocess.run(full_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    if result.returncode != 0:
+        print("FAILED - {}".format(" ".join(cmd)))
+        print("         {}".format(result.stdout.decode()))
+        print("         {}".format(result.stderr.decode()))
     return result
 
 
-def run_tf_cnn_benchmark(model, options):
+def run_tf_cnn_benchmark(model, options, env_vars):
 
     run_benchmark_cmd = [
         "python3",
@@ -34,7 +38,7 @@ def run_tf_cnn_benchmark(model, options):
     for option in options:
         run_benchmark_cmd.append(option)
 
-    result = run_shell_command(run_benchmark_cmd)
+    result = run_shell_command(run_benchmark_cmd, env_vars)
     return (result.returncode, result.stdout.decode(), result.stderr.decode())
 
 
@@ -42,11 +46,11 @@ def run_tf_cnn_benchmark(model, options):
 def collect_tf_cnn_benchmark_perf_data():
 
     models = [
-        "alexnet",
-        "googlenet",
-        "inception3",
-        "inception4",
-        "lenet",
+        # "alexnet",
+        # "googlenet",
+        # "inception3",
+        # "inception4",
+        # "lenet",
         # "mobilenet",
         # "nasnet",
         # "nasnetlarge",
@@ -54,17 +58,22 @@ def collect_tf_cnn_benchmark_perf_data():
         # "overfeat",
         "resnet50",
         "resnet50_v1.5",
-        "resnet101",
-        "resnet152_v2",
+        # "resnet101",
+        # "resnet152_v2",
         # "trivial",
-        "vgg11",
-        "vgg16",
-        "vgg19",
+        # "vgg11",
+        # "vgg16",
+        # "vgg19",
     ]
 
     configs = [
-        ("xla_OFF", []),
-        ("xla_ON", ["--xla"]),
+        ("baseline", ["--batch_size=64"], {"TF_ROCM_FMA_DISABLE":"1",}),
+        # ("XLA", ["--batch_size=64", "--xla"], {"TF_ROCM_FMA_DISABLE":"1",}),
+        ("AMP", ["--batch_size=64", "--auto_mixed_precision"], {"TF_ROCM_FMA_DISABLE":"1",}),
+        # ("FP16", ["--batch_size=64", "--use_fp16"], {"TF_ROCM_FMA_DISABLE":"1",}),
+        ("FUSION", ["--batch_size=64"], {"TF_ROCM_FUSION_ENABLE":"1",}),
+        ("AMP + FUSION", ["--batch_size=64", "--auto_mixed_precision"], {"TF_ROCM_FUSION_ENABLE":"1",}),
+        # ("AMP + XLA", ["--batch_size=64", "--auto_mixed_precision", "--xla"], {"TF_ROCM_FMA_DISABLE":"1",}),
     ]
     
     N = 3
@@ -74,16 +83,16 @@ def collect_tf_cnn_benchmark_perf_data():
     data["configs"] = configs
     data["N"] = N
     
-    def run_N_times(N, model, options=[]):
+    def run_N_times(N, model, options=[], env_vars = {}):
         config_data = {}
         for i in range(N):
-            run_data = run_tf_cnn_benchmark(model, options)
+            run_data = run_tf_cnn_benchmark(model, options, env_vars)
             config_data["run_{}".format(i)] = run_data
         return config_data
 
     for model in models:
-        for name, options in configs:
-            config_data = run_N_times(N, model, options)
+        for name, options, env_vars in configs:
+            config_data = run_N_times(N, model, options, env_vars)
             data["{}_{}".format(model, name)] = config_data
 
     return data
@@ -115,7 +124,7 @@ def gather_stats(data):
     
     for model in models:
         model_stats = {}
-        for name, options in configs:
+        for name, options, env_vars in configs:
             config_data = data["{}_{}".format(model, name)]
             model_stats[name] = get_stats(N, config_data)
         stats[model] = model_stats
@@ -136,33 +145,78 @@ def dump_stats(excel_file, stats):
     N = stats["N"]
     
     header_1 = ["", ""]
-    for name, options in configs:
+    for name, options, env_vars in configs:
         header_1.append(name)
         for i in range(N):
             header_1.append("")
         header_1.append("")
-    # print (header_1)
+    print (header_1)
     sheet.append(header_1)
     
     header_2 = ["benchmark", ""]
-    for name, options in configs:
+    for name, options, env_vars in configs:
         for i in range(N):
             header_2.append("run_{}".format(i))
         header_2.append("AVERAGE") 
         header_2.append("") 
-    # print (header_2)
+    print (header_2)
     sheet.append(header_2)
 
     for model in models:
         row = [model, ""]
         model_stats = stats[model]
-        for name, options in configs:
+        for name, options, env_vars in configs:
             config_stats = model_stats[name]
             for i in range(N):
                 row.append(config_stats[i])
             row.append("")
             row.append("")
-        # print(row)
+        print(row)
+        sheet.append(row)
+            
+    workbook.save(filename=excel_file)
+
+
+def dump_stats_transposed(excel_file, stats):
+
+    workbook = Workbook()
+    sheet = workbook.active
+
+    workbook = Workbook()
+    sheet = workbook.active
+
+    models = stats["models"]
+    configs = stats["configs"]
+    N = stats["N"]
+    
+    header_1 = ["", ""]
+    for name, options, env_vars in configs:
+        header_1.append(name)
+        for i in range(N):
+            header_1.append("")
+        header_1.append("")
+    print (header_1)
+    sheet.append(header_1)
+    
+    header_2 = ["benchmark", ""]
+    for name, options, env_vars in configs:
+        for i in range(N):
+            header_2.append("run_{}".format(i))
+        header_2.append("AVERAGE") 
+        header_2.append("") 
+    print (header_2)
+    sheet.append(header_2)
+
+    for model in models:
+        row = [model, ""]
+        model_stats = stats[model]
+        for name, options, env_vars in configs:
+            config_stats = model_stats[name]
+            for i in range(N):
+                row.append(config_stats[i])
+            row.append("")
+            row.append("")
+        print(row)
         sheet.append(row)
             
     workbook.save(filename=excel_file)
@@ -180,6 +234,7 @@ if __name__ == '__main__':
             data = json.loads(f.read())
             stats = gather_stats(data)
             dump_stats(filename.replace("json", "xlsx"), stats)
+            # dump_stats_transposed(filename.replace("json", "xlsx"), stats)
 
     else :
         data = collect_tf_cnn_benchmark_perf_data()
