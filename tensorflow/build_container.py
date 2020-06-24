3d31aed282ccc6b0d01eaea060f047b7ec00a04e
Deven Desai <deven.desai.amd@gmail.com>
2020-06-24 15:01:22 -0700

    prj47-rack-15 update

--- tensorflow/build_container.py
+++ tensorflow/build_container.py
@@ -11,89 +11,70 @@ TF_REPO_LOC = "/home/deven/deven/repos/tensorflow-upstream"
 
 
 def run_shell_command(cmd, workdir):
-
     cwd = os.getcwd()
     os.chdir(workdir)
-
     result = subprocess.run(cmd)
     if result.returncode != 0:
         sys.exit(result.returncode)
-
     os.chdir(cwd)
     return result.returncode
 
 
-def get_docker_config(tag):
-    docker_image = "devenamd/tensorflow:{}-{}".format(tag, date.today().strftime("%y%m%d"))
-    docker_file = os.path.join(TF_REPO_LOC, "tensorflow/tools/ci_build/Dockerfile.rocm")
-    return docker_image, docker_file, None
+def get_release_build():
+    docker_image_tag = "rocm35-tfdu"
+    return docker_image_tag, None
 
 
-def get_docker_config_rc_build(tag):
-    docker_image = "devenamd/tensorflow:{}-{}".format(tag, date.today().strftime("%y%m%d"))
-    docker_file = os.path.join(TF_REPO_LOC, "tensorflow/tools/ci_build/Dockerfile.rocm-rc")
-    return docker_image, docker_file, None
-
-
-def get_docker_config_hipclang_internal_build(tag, internal_build_number):
-    build_name="compute-rocm-dkms-no-npi-hipclang-{}".format(internal_build_number)
-    docker_image = "devenamd/tensorflow:{}-{}".format(build_name, date.today().strftime("%y%m%d"))
-    docker_file = os.path.join(TF_REPO_LOC, "tensorflow/tools/ci_build/Dockerfile.rocm-internal")
+def get_rc_build():
+    version = "3.6"
+    release = "rel-22"
+    install_dir = "rocm-3.6.0-22"
+    docker_image_tag = "rocm36_rc1-tfdu"
     docker_build_args = [
-        "--build-arg", "ROCM_INTERNAL_ARTIFACTORY=http://compute-artifactory.amd.com/artifactory/list/rocm-osdb-deb/",
-        "--build-arg", "ROCM_BUILD_NAME=compute-rocm-dkms-no-npi-hipclang",
-        "--build-arg", "ROCM_BUILD_NUM={}".format(internal_build_number),
+        "--build-arg", "ROCM_DEB_REPO=http://compute-artifactory.amd.com/artifactory/list/rocm-release-archive-deb/",
+        "--build-arg", "ROCM_BUILD_NAME={}".format(version),
+        "--build-arg", "ROCM_BUILD_NUM={}".format(release),
+        "--build-arg", "ROCM_PATH=/opt/{}".format(install_dir),
         ]
-    return docker_image, docker_file, docker_build_args
+    return docker_image_tag, docker_build_args
 
 
-def get_docker_config_hipclang_bkc_build(tag, bkc_major, bkc_minor):
-    build_name="compute-rocm-dkms-no-npi-hipclang-int-bkc-{}-{}".format(bkc_major, bkc_minor)
-    docker_image = "devenamd/tensorflow:{}-{}".format(build_name, date.today().strftime("%y%m%d"))
-    docker_file = os.path.join(TF_REPO_LOC, "tensorflow/tools/ci_build/Dockerfile.rocm-internal")
+def get_bkc_build():
+    bkc_major = 0
+    bkc_minor = 0
+    docker_image_tag = "rocm35_{}-tfdu".format(internal_build_number)
     docker_build_args = [
-        "--build-arg", "ROCM_INTERNAL_ARTIFACTORY=http://compute-artifactory.amd.com/artifactory/list/rocm-osdb-deb/",
+        "--build-arg", "ROCM_DEB_REPO=http://compute-artifactory.amd.com/artifactory/list/rocm-osdb-deb/",
         "--build-arg", "ROCM_BUILD_NAME=compute-rocm-dkms-no-npi-hipclang-int-bkc-{}".format(bkc_major),
         "--build-arg", "ROCM_BUILD_NUM={}".format(bkc_minor),
         ]
-    return docker_image, docker_file, docker_build_args
-
-
-def develop_upstream_hipclang_internal_build():
-    tf_branch = "develop-upstream"
+    return docker_image_tag, docker_build_args
+    
+    
+def get_internal_build():
     internal_build_number = 2363
-    shutil.copy("/home/deven/deven/common/scripts/tensorflow/Dockerfile.rocm-internal", os.path.join(TF_REPO_LOC, "tensorflow/tools/ci_build"))
-    return get_docker_config_hipclang_internal_build(tf_branch, internal_build_number)
-
-
-def upstream_r21_build():
-    tf_branch = "r2.1_rocm33"
-    return get_docker_config(tf_branch)
-
-
-def develop_upstream_rocm35_rc_build():
-    tag = "rocm35_rc3"
-    shutil.copy("/home/deven/deven/common/scripts/tensorflow/Dockerfile.rocm-rc", os.path.join(TF_REPO_LOC, "tensorflow/tools/ci_build"))
-    return get_docker_config_rc_build(tag)
-
-
-def develop_upstream_build():
-    tag = "rocm35"
-    return get_docker_config(tag)
+    docker_image_tag = "rocm35_{}-tfdu".format(internal_build_number)
+    docker_build_args = [
+        "--build-arg", "ROCM_DEB_REPO=http://compute-artifactory.amd.com/artifactory/list/rocm-osdb-deb/",
+        "--build-arg", "ROCM_BUILD_NAME=compute-rocm-dkms-no-npi-hipclang",
+        "--build-arg", "ROCM_BUILD_NUM={}".format(internal_build_number),
+        ]
+    return docker_image_tag, docker_build_args
 
 
 if __name__ == '__main__':
     
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("commit")
-    # args = parser.parse_args()
-    # commit = args.commit
-
-    docker_image, docker_file, docker_build_args = develop_upstream_build()
-    
+    docker_file = os.path.join(TF_REPO_LOC, "tensorflow/tools/ci_build/Dockerfile.rocm")
     docker_context = os.path.join(TF_REPO_LOC, "tensorflow/tools/ci_build")
 
-    docker_build_command = ["docker", "build", "-t", docker_image, "-f", docker_file]
+    # docker_image_tag, docker_build_args = get_release_build()
+    docker_image_tag, docker_build_args = get_rc_build()
+    # docker_image_tag, docker_build_args = get_bkc_build()
+    # docker_image_tag, docker_build_args = get_internal_build()
+
+    docker_image_name = "devenamd/tensorflow:{}-{}".format(docker_image_tag, date.today().strftime("%y%m%d"))
+    
+    docker_build_command = ["docker", "build", "-t", docker_image_name, "-f", docker_file]
     if docker_build_args is not None:
         docker_build_command.extend(docker_build_args)
     docker_build_command.append(docker_context)
