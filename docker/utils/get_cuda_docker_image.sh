c004fe952623ee7fc4c4fad13f4950ac5ac5795b
Deven Desai <deven.desai.amd@gmail.com>
2021-09-23 12:39:14 -0400

    ixt-rack-61 update

--- docker/utils/get_cuda_docker_image.sh
+++ docker/utils/get_cuda_docker_image.sh
@@ -1,19 +1,28 @@
 
-docker_repo=tensorflow/tensorflow
+# docker_repo=tensorflow/tensorflow
 # docker_repo=devenamd/tensorflow
 
 # tag=1.12.0-devel-gpu-py3
 # tag=cuda_190513
-tag=latest-devel-gpu-py3
+# tag=latest-devel-gpu-py3
 
 docker_repo=nvcr.io/nvidia/tensorflow
-tag=20.03-tf2-py3
+tag=21.08-tf2-py3
 
-container_name=deven_tf_trial
+container_name=deven_01_TF_RESNET50
 
-sudo docker run \
-     -it \
-     -p 8888:8888 \
-     -v $HOME/deven/common:/common \
-     --name $container_name \
-     $docker_repo:$tag
+options=""
+options="$options -it"
+options="$options --gpus \"device=0,1,2,3,4,5,6,7\""
+options="$options --network=host"
+options="$options --ipc=host"
+options="$options --shm-size 16G"
+options="$options --cap-add=SYS_PTRACE"
+options="$options --security-opt seccomp=unconfined"
+
+options="$options -v $HOME/deven/common:/common"
+options="$options -v /data-bert:/data-bert"
+options="$options -v /data/imagenet:/data/imagenet"
+# options="$options -v /data/imagenet-inception:/data/imagenet-inception:"
+
+sudo docker run $options --name $container_name $docker_repo:$tag
